0201ff0217792c1a4e2925b2842fb022df65e748
PoojaDornal <37832052+PoojaDornal@users.noreply.github.com>
2024-01-26 16:37:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import  ExpressionForm from './Components/Expression.js';
+
+function App() {
+  return (
+    <div className="pb-5 pt-3 bd-dark">
+      <h2 className='text-center mt-2 mb-4'>Expression Engine!</h2>
+       <ExpressionForm />
+    </div>
+  );
+}
+
+export default App;
